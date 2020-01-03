3ed4bed7a5f115279dfaa98c3ba2980eb9583c94
wolfjosiah <wolfjosiah@users.github.com>
2020-01-03 03:37:24 -0800

    Users can be added to a game.
    Users are represented as a string of their user id for now.

--- game.js
+++ game.js
@@ -1,5 +1,17 @@
 
-module.exports = class Game
+class Game
 {
-  
+  players = new Set();
+
+  add_player(user)
+  {
+    this.players.add(user);
+  }
+
+  has_player(user)
+  {
+    return this.players.has(user);
+  }
 }
+
+module.exports = Game;
--- test.js
+++ test.js
@@ -4,4 +4,16 @@
   const Game = require('./game');
 
   const game = new Game();
+
+  const user = "user id";
+
+  game.add_player(user);
+  console.assert(
+    game.has_player(user),
+    "The added user is in the game.");
+
+  const other_user = "user id 2";
+  console.assert(
+    !game.has_player(other_user),
+    "The user that hasn't been added isn't in the game.");
 })();
